157bd54941ddafd4d9f12f203e376a6df3a97c6b
Benjamin Scheidt <benjamin.scheidt@informatik.hu-berlin.de>
2020-06-04 10:50:47 +0200

    move code in own class

--- echo_server2.py
+++ echo_server2.py
@@ -1,86 +1,30 @@
-import selectors
-import socket
-import types
 import logging as log
 import tkinter as tk
-import threading
 
-from util import *
-from connection import Connection
-
+from network import NetworkThread
 
 VERSION = 0.1
 log.basicConfig(level=log.DEBUG, filename='debug.log')
 
-TIMEOUT = 5.0
-DEFAULT_HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-DEFAULT_PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
-RUNNING = True
-CONN = {}  # Host|Port -> Connection
+if __name__ == "__main__":  # called as script, not as module
+    
+    def start():
+        global nt
+        nt = NetworkThread(hostname_entry.get(), int(port_entry.get()))
+        nt.start()
+    def stop():
+        global nt
+        if nt is not None:
+            nt.stop()
 
-def accept_wrapper(sel, sock):
-    conn, addr = sock.accept()  # Should be ready to read
-    log.debug('accepted connection from %s' % str(addr))
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
-
-    ident = get_key(conn)
-    CONN[ident] = Connection(CONN, ident, conn)
-
-def service_connection(sel, key, mask):
-    sock = key.fileobj
-    data = key.data
-    ident = get_key(sock)
-    connection = CONN[ident]
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data:
-            connection.handle_data(recv_data)
-        else:  # connection closed
-            log.debug('closing connection to %s' % str(data.addr))
-            sel.unregister(sock)
-            sock.close()
-            del CONN[ident]
-    if mask & selectors.EVENT_WRITE:
-        if connection.has_data():
-            log.debug('echoing %s to %s' % (repr(connection.out), data.addr))
-            sent = sock.send(connection.out)  # Should be ready to write
-            connection.out = connection.out[sent:]
-
-def networking(host, port):
-    print("Starting server on (%s, %s)" % (host, port))
-    sel = selectors.DefaultSelector()
-    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    lsock.bind((host, port))
-    lsock.listen()
-    log.debug('listening on (%s, %s)' % (host, port))
-    lsock.setblocking(False)
-    sel.register(lsock, selectors.EVENT_READ, data=None)
-    while RUNNING:
-        events = sel.select(timeout=TIMEOUT)
-        for key, mask in events:
-            if key.data is None:
-                accept_wrapper(sel, key.fileobj)
-            else:
-                service_connection(sel, key, mask)
-    lsock.close()
-    print("Stopping server")
-
-def start_network(host, port):
-    global RUNNING
-    RUNNING = True
-    thread = threading.Thread(target = networking, args=[host, port])
-    thread.deamon = True
-    thread.start()
-
-def stop_network():
-    global RUNNING
-    RUNNING = False
-
-if __name__ == "__main__":
+    def on_close():
+        if nt is not None:
+            nt.stop()
+        root.destroy()
+    
     root=tk.Tk()
+    root.protocol("WM_DELETE_WINDOW", on_close)
+    nt = None
 
     frame=tk.Frame(root,width=500,height=450)
 
@@ -91,16 +35,16 @@ if __name__ == "__main__":
     port_label.grid(row=1,sticky=tk.E)
 
     hostname_entry=tk.Entry(frame)
-    hostname_entry.insert(tk.END, DEFAULT_HOST)
+    hostname_entry.insert(tk.END, NetworkThread.DEFAULT_HOST)
     port_entry=tk.Entry(frame)
-    port_entry.insert(tk.END, DEFAULT_PORT)
+    port_entry.insert(tk.END, NetworkThread.DEFAULT_PORT)
 
     hostname_entry.grid(row=0,column=1)
     port_entry.grid(row=1,column=1)
 
-    button1=tk.Button(frame,text="Start Server", command=lambda: start_network(hostname_entry.get(), int(port_entry.get())))        
+    button1=tk.Button(frame,text="Start Server", command=start)
     button1.grid(row=2,column=0)
-    button3=tk.Button(frame,text="Stop Server", command=stop_network)
+    button3=tk.Button(frame,text="Stop Server", command=stop)
     button3.grid(row=2,column=1)
     frame.grid()
 
---
+++ network.py
@@ -0,0 +1,88 @@
+import selectors
+import socket
+import threading
+import types
+import logging as log
+
+import util
+from connection import Connection
+
+
+class NetworkThread(threading.Thread):
+    TIMEOUT = 1
+    DEFAULT_HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+    DEFAULT_PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+    def __init__(self, host=DEFAULT_HOST, port=DEFAULT_PORT):
+        threading.Thread.__init__(self)
+        self.running = True
+        self.host = host
+        self.port = port
+        self.lsock = None
+        self.sel = None
+        self.connections = {}
+
+    def set_connection_pair(self, host, port):
+        self.host = host
+        self.port = port
+
+    def run(self):
+        print("Starting server on (%s, %s)" % (self.host, self.port))
+        self.sel = selectors.DefaultSelector()
+        self.lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.lsock.bind((self.host, self.port))
+        self.lsock.listen()
+        self.lsock.setblocking(False)
+        self.sel.register(self.lsock, selectors.EVENT_READ, data=None)
+        log.debug('listening on (%s, %s)' % (self.host, self.port))
+
+        while self.running:
+            events = self.sel.select(timeout=self.TIMEOUT)
+            for key, mask in events:
+                if key.data is None:
+                    self.accept_wrapper(key.fileobj)
+                else:
+                    self.service_connection(key, mask)
+        self.lsock.close()
+        self.sel.close()
+        self.reset()
+        print("Stopping server")
+
+    def reset(self):
+        self.lsock = None
+        self.sel = None
+        self.connections = {}
+
+    def stop(self):
+        self.running = False
+
+    def accept_wrapper(self, sock):
+        conn, addr = sock.accept()  # Should be ready to read
+        log.debug('accepted connection from %s' % str(addr))
+        conn.setblocking(False)
+        data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        self.sel.register(conn, events, data=data)
+
+        ident = util.get_key(conn)
+        self.connections[ident] = Connection(self.connections, ident, conn)
+
+    def service_connection(self, key, mask):
+        sock = key.fileobj
+        data = key.data
+        ident = util.get_key(sock)
+        connection = self.connections[ident]
+        if mask & selectors.EVENT_READ:
+            recv_data = sock.recv(1024)  # Should be ready to read
+            if recv_data:
+                connection.handle_data(recv_data)
+            else:  # connection closed
+                log.debug('closing connection to %s' % str(data.addr))
+                self.sel.unregister(sock)
+                sock.close()
+                del self.connections[ident]
+        if mask & selectors.EVENT_WRITE:
+            if connection.has_data():
+                log.debug('echoing %s to %s' % (repr(connection.out), data.addr))
+                sent = sock.send(connection.out)  # Should be ready to write
+                connection.out = connection.out[sent:]
