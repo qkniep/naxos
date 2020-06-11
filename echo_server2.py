5bda4c925cf96607466aca1765c49155c0bd2511
Quentin Kniep <hello@quentinkniep.com>
2020-06-11 13:46:19 +0200

    CLI version of Naxos

--- echo_server2.py
+++ echo_server2.py
@@ -1,6 +1,7 @@
 import threading
 import logging as log
-import tkinter as tk
+import sys
+import socket
 
 from network import NetworkThread
 import util
@@ -9,23 +10,23 @@ VERSION = 0.1
 log.basicConfig(level=log.DEBUG, filename='debug.log')
 
 if __name__ == "__main__":  # called as script, not as module
-    
-    def start():
+
+    def start(ip, port):
         global nt, queue
         queue = util.PollableQueue()
-        nt = NetworkThread(queue, hostname_entry.get(), int(port_entry.get()))
+        nt = NetworkThread(queue, ip, int(port))
         nt.start()
-    
+
     def stop():
         global nt
         if nt is not None:
             nt.stop()
-    
-    def connect():
+
+    def connect(ip, port):
         global queue
-        queue.put(("connect", {
-            "host": hostname_connect_entry.get(),
-            "port": int(port_connect_entry.get())
+        queue.put(('connect', {
+            'host': ip,
+            'port': int(port),
         }))
 
     def on_close():
@@ -34,60 +35,11 @@ if __name__ == "__main__":  # called as script, not as module
             while not nt.is_done():  # blocking wait
                 pass
         root.destroy()
-    
-    root=tk.Tk()
-    root.title("Naxos %s" % VERSION)
 
-    root.protocol("WM_DELETE_WINDOW", on_close)
-    nt = None
-    queue = None
-
-    frame=tk.Frame(root,width=500,height=450)
-    frame2 = tk.Frame(root, width=500, height=450)
-
-
-    #  Frame 1, own listening port
-    hostname_label=tk.Label(frame, text="Hostname")
-    port_label=tk.Label(frame, text="Port")
-
-    hostname_label.grid(row=0,sticky=tk.E)
-    port_label.grid(row=1,sticky=tk.E)
-
-    hostname_entry=tk.Entry(frame)
-    hostname_entry.insert(tk.END, NetworkThread.DEFAULT_HOST)
-    port_entry=tk.Entry(frame)
-    port_entry.insert(tk.END, NetworkThread.DEFAULT_PORT)
-
-    hostname_entry.grid(row=0,column=1)
-    port_entry.grid(row=1,column=1)
-
-    button1=tk.Button(frame,text="Start Server", command=start)
-    button1.grid(row=2,column=0)
-    button3=tk.Button(frame,text="Stop Server", command=stop)
-    button3.grid(row=2,column=1)
-
-
-    #  Frame 2, connect to other peer
-    host_connect_label=tk.Label(frame2, text="Hostname")
-    port_connect_label=tk.Label(frame2, text="Port")
-
-    host_connect_label.grid(row=0,sticky=tk.E)
-    port_connect_label.grid(row=1,sticky=tk.E)
-
-    hostname_connect_entry=tk.Entry(frame2)
-    hostname_connect_entry.insert(tk.END, NetworkThread.DEFAULT_HOST)
-    port_connect_entry=tk.Entry(frame2)
-    port_connect_entry.insert(tk.END, NetworkThread.DEFAULT_PORT)
-
-    hostname_connect_entry.grid(row=0,column=1)
-    port_connect_entry.grid(row=1,column=1)
-
-    button1=tk.Button(frame2,text="Connect", command=connect)
-    button1.grid(row=2,column=1)
-
-
-
-    frame.grid(row=0,column=0)
-    frame2.grid(row=0,column=1)
-
-    root.mainloop()
+    if sys.argv[1] != 'server':
+        start(sys.argv[1], sys.argv[2])
+        connect(sys.argv[3], sys.argv[4])
+    elif len(sys.argv) == 2:
+        start(NetworkThread.DEFAULT_HOST, NetworkThread.DEFAULT_PORT)
+    elif len(sys.argv) == 4:
+        start(sys.argv[2], sys.argv[3])
