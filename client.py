a6fc3859d00ca0f1b57db3b2f0aced6c6b0f7d94
Benjamin Scheidt <benjamin.scheidt@informatik.hu-berlin.de>
2020-06-22 19:19:51 +0200

    implement client: send search/download requests send insert/remove requests

--- client.py
+++ client.py
@@ -1,19 +1,113 @@
 import configparser
 import os
 import re
+import selectors
+import shlex
+import socket
 import sys
 import threading
+import time
 
 from pathlib import Path
 
+import util
+from connection import Connection
+from message import Message
 
-if __name__ == "__main__":
+
+SELECT_TIMEOUT = 2
+RECV_BUFFER = 1024
+
+class InputThread(threading.Thread):
+
+    def __init__(self, queue):
+        super().__init__()
+
+        self.queue = queue
+        self.running = True
+
+    def run(self):
+        while self.running:
+            try:
+                line = input("> ")
+            except EOFError:
+                print("Encountered EOF in stdin ಠ_ಠ")
+                exit(0)
+            if line == "":
+                continue
+            cmd, *args = shlex.split(line)
+            
+            if cmd == "search" or cmd == "s":
+                if len(args) == 0:
+                    print("Usage: >search <filename1> <filename2> ...")
+                    continue
+                
+                self.queue.put(("search", {
+                    "files": args
+                }))
+            elif cmd == "download" or cmd == "d":
+                if len(args) == 0:
+                    print("Usage: >download <filename1> <filename2> ...")
+                    continue
+
+                self.queue.put(("download", {
+                    "files": args
+                }))
+            elif cmd == "quit":
+                self.queue.put(("quit", {}))
+            else:
+                print("Usage: >(search|download) <filename1> <filename2> ...")
+
+    def stop(self):
+        self.running = False
+
+
+class DirectoryObserver(threading.Thread):
+    CHECK_INTERVAL = 1
+
+    def __init__(self, path, queue):
+        super().__init__()
+
+        self.queue = queue
+        self.path = path
+        self.running = True
+
+    def run(self):
+        scan = lambda p: set([f.name for f in p.glob("*") if f.is_file()])
+        # files = set([f.name for f in self.path.glob("*") if f.is_file()])  # list (non-recursive) all files in directory
+        files = scan(self.path)
+        old = files
+        self.queue.put(("insert", {
+            "files": list(files)
+        }))
+        while self.running:
+            new = files - old
+            removed = old - files
+            if new:  # new files detected
+                print("new: %s" % new)
+                self.queue.put(("insert", {
+                    "files": list(new)
+                }))
+            if removed:  # removed files detected
+                print("delete: %s" % removed)
+                self.queue.put(("delete", {
+                    "files": list(removed)
+                }))
+            old = files
+            files = scan(self.path)
+            time.sleep(self.CHECK_INTERVAL)
+
+    def stop(self):
+        self.running = False
+
+
+def parse_config(argv):
     config_path = Path.cwd() / "naxos.ini"
     host = None
     port = None
     naxos_path = None
     
-    if len(sys.argv) == 1 and config_path.is_file():  # try to read the provided config file
+    if len(argv) == 1 and config_path.is_file():  # try to read the provided config file
         try:
             config = configparser.ConfigParser()
             config.read(config_path)
@@ -26,8 +120,8 @@ if __name__ == "__main__":
             exit(0)
     else:  # try to read the necessary parameters from args
         try:
-            host, port = sys.argv[1].split(":")
-            naxos_path = Path(sys.argv[2])
+            host, port = argv[1].split(":")
+            naxos_path = Path(argv[2])
         except:
             print('Usage: python client.py <naxos peer host>:<naxos peer port> <"path to naxos directory">')
             print('Alternative: python client.py with a naxos.ini file in the same directory')
@@ -47,8 +141,87 @@ if __name__ == "__main__":
     elif not (1024 <= int(port) <= 65535):
         print("provided port is not a valid port.")
         exit(0)
-
     port = int(port)
+    addr = (host, port)
 
-    print("%s:%s" % (host, port))
-    print(naxos_path)
+    return addr, naxos_path
+
+
+def handle_queue(queue, sock, conn):
+    cmd, payload = queue.get()
+    if cmd == "quit":
+        dir_observer.stop()
+        input_thread.stop()
+        sock.close()
+        exit(0)
+
+    elif cmd == "insert":
+        conn.send(Message({
+            "do": "client_insert",
+            "files": payload["files"]
+        }))
+    elif cmd == "delete":
+        conn.send(Message({
+            "do": "client_delete",
+            "files": payload["files"]
+        }))
+
+    elif cmd == "search":
+        conn.send(Message({
+            "do": "client_search",
+            "files": payload["files"]
+        }))
+    elif cmd == "download":
+        conn.send(Message({
+            "do": "client_download",
+            "files": payload["files"]
+        }))
+    else:
+        raise ValueError("Unexpected command.")
+
+
+if __name__ == "__main__":
+    addr, naxos_path = parse_config(sys.argv)
+
+    selector = selectors.DefaultSelector()
+
+    queue = util.PollableQueue()
+    selector.register(queue, selectors.EVENT_READ)
+
+    input_thread = InputThread(queue)
+    input_thread.start()
+    dir_observer = DirectoryObserver(naxos_path, queue)
+    dir_observer.start()
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(addr)
+        selector.register(sock, selectors.EVENT_READ | selectors.EVENT_WRITE)
+    except (ConnectionRefusedError, ConnectionAbortedError, TimeoutError) as e:
+        print('Could not establish connection to (%s, %s):' % addr, e)
+        exit(0)
+    conn = Connection(sock, known=True)
+
+    while True:
+        events = selector.select(timeout=SELECT_TIMEOUT)
+        for key, mask in events:
+            if key.fileobj is queue:
+                handle_queue(queue, sock, conn)
+            elif key.fileobj is sock:
+                if mask & selectors.EVENT_READ:
+                    try:
+                        recv_data = sock.recv(RECV_BUFFER)
+                        if recv_data:
+                            print(recv_data)
+                            # TODO: handle data properly
+                        else:  # connection closed
+                            selector.unregister(sock)
+                            sock.close()
+                            exit(0)
+                    except (ConnectionResetError, ConnectionAbortedError):
+                        print('Connection reset/aborted:', addr)
+                        selector.unregister(sock)
+                        sock.close()
+                        exit(0)
+                if mask & selectors.EVENT_WRITE:
+                    conn.flush_out_buffer()
